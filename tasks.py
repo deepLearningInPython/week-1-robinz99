ca14b3510870c635d34e14df03df7e7d9dcdf843
robinz99 <robin.zehrer@student.uva.nl>
2024-11-04 21:28:39 +0100

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -13,7 +13,11 @@ import numpy
 # Your code here:
 # -----------------------------------------------
 
-def step
+def step(x):
+    if x > 0:
+        return 1
+    else:
+        return -1
 
 
 # -----------------------------------------------
@@ -28,7 +32,8 @@ def step
 
 # Your code here:
 # -----------------------------------------------
-def ReLu
+def ReLu(array, cutoff=0):
+    return numpy.where(array < cutoff, cutoff, array)
 
 
 # -----------------------------------------------
@@ -44,7 +49,8 @@ def ReLu
 # Your code here:
 # -----------------------------------------------
 
-def neural_net_layer
+def neural_net_layer(matrix, vector):
+    return ReLu(matrix * vector)
 
 
 # ------------------------------------------
